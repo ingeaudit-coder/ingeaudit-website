a5e9ba381a3507b3ad4822684a66b90e5d5c8cca
gong <elias.gonzaloguerrero@gmail.com>
2026-01-28 17:19:19 -0300

    Refactor componente Hero para mejorar la estructura y estilos CSS

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -1,13 +1,25 @@
-import style from "@/src/components/sections/Hero/Hero.module.css"
+import style from "@/src/components/sections/Hero/Hero.module.css";
+import Image from "next/image";
 
-const Hero =()=>{
-    return(
-        <main className={style.heroContainer}>
-        <section>
-            <p>LIDERES EN TELECOMUNICACIONES</p>
-        </section>
-        </main>
-    )
-}
+const Hero = () => {
+  return (
+    <main className={style.heroContainer}>
+        <Image
+        src="/bg-img-hero.webp"
+        alt="Fondo corporativo"
+        fill
+        priority
+        className="object-cover"
+      />
+      <div className={style.overlay} />
+      <section className={style.content}>
+        <h1 className={style.titleHero}>Lideres en Consultoría Técnica-Regulatoria</h1>
+        <p className={style.subtitleHero}>
+          Más de <strong><b>20 años de experiencia</b></strong> respaldando a la industria de las telecomunicaciones
+        </p>
+      </section>
+    </main>
+  );
+};
 
 export default Hero;
