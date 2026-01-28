c34e03ce1456cf8c815573eb238cbc897e7b3f73
gong <elias.gonzaloguerrero@gmail.com>
2026-01-28 11:32:57 -0300

    Agregar componente Hero con estilos y contenido inicial

---
+++ src/components/sections/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import style from "@/src/components/sections/Hero/Hero.module.css"
+
+const Hero =()=>{
+    return(
+        <main className={style.heroContainer}>
+        <section>
+            <p>LIDERES EN TELECOMUNICACIONES</p>
+        </section>
+        </main>
+    )
+}
+
+export default Hero;
