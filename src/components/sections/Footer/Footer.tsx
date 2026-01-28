8f95c3011da93a3c9fd5a982412fa057c2b294e1
gong <elias.gonzaloguerrero@gmail.com>
2026-01-28 17:18:46 -0300

    Agregar componente Footer con estilos CSS

---
+++ src/components/sections/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import style from "@/src/components/sections/Footer/Footer.module.css"
+
+const Footer =()=>{
+    return(
+        <section className={style.footerContainer}>
+            <p>Política de Seguridad</p>
+        </section>
+    )
+}
+
+export default Footer;
