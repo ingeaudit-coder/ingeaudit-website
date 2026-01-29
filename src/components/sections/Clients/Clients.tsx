9b826b49e958a6756cfd763ca2a3f33a370f483a
gong <elias.gonzaloguerrero@gmail.com>
2026-01-29 16:41:57 -0300

    Agregar componente Clients con estilos CSS

---
+++ src/components/sections/Clients/Clients.tsx
@@ -0,0 +1,11 @@
+import style from "@/src/components/sections/Clients/Clients.module.css"
+
+const Clients =()=>{
+    return(
+        <section className={style.clientsContainer}>
+            <h2>Nuestro Clientes</h2>
+        </section>
+    )
+}
+
+export default Clients;
