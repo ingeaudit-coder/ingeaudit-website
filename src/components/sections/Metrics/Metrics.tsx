7341d5b351bed5a83537d4c4e748a4a0cc12b454
gong <elias.gonzaloguerrero@gmail.com>
2026-02-10 13:36:17 -0300

    se agrego un nuevo componente Metric, con datos numericos de los servicios usado con "react-countup"

--- src/components/sections/Metrics/Metrics.tsx
+++ src/components/sections/Metrics/Metrics.tsx
@@ -1,23 +1,45 @@
-import style from "@/src/components/sections/Metrics/Metrics.module.css"
+import style from "@/src/components/sections/Metrics/Metrics.module.css";
+import CountUp from "react-countup";
 
-const Metrics =()=> {
-  return ( 
+const Metrics = () => {
+  return (
     <section className={style.containerMetrics}>
-        <div className={style.metricasAntenas}>
-            <h3>5k+</h3>
-            <p>Más de 5 mil antenas medidas en todo Chile</p>
+      <div className={style.previsitasMetricas}>
+        <div className={style.linePrevisitas}>
+          <CountUp
+            start={100000}
+            end={138747}
+            duration={3}
+            className={style.preCount}
+          />
+          <p className={style.subtitleMetrics}>Previsitas a la fecha</p>
         </div>
-        <div className={style.homoMetricas}>
-            <h3>834945</h3>
-            <p>Equipos homologados a la fecha</p>
+      </div>
 
+      <div className={style.metricasAntenas}>
+        <div className={style.lineAntenas}>
+          <h3 className={style.numberAntenas}>5k+</h3>
+          <p className={style.subtitleMetrics}>
+            Más de 5 mil antenas medidas en todo Chile
+          </p>
         </div>
-        <div className={style.previsitasMetricas}>
-            <h3>431</h3>
-            <p>Previsitas a la fecha</p>
+      </div>
+
+      <div className={style.homoMetricas}>
+        <div className={style.lineHomo}>
+          <CountUp
+            start={700000}
+            end={834945}
+            duration={3}
+            className={style.homoCount}
+          />
+          <p className={style.subtitleMetrics}>
+            Equipos homologados a la fecha
+          </p>
         </div>
+      </div>
     </section>
-  )
+  );
 };
 
 export default Metrics;
