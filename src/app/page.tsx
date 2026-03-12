520dc4f50519870f84e5848473e258665b8b064d
gong <elias.gonzaloguerrero@gmail.com>
2026-03-12 17:18:33 -0300

    Reactivar componentes Clients, Servicios, Metrics, Certifications y Footer en la página de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import Certifications from "../components/sections/Certifications/Certifications";
 import Clients from "../components/sections/Clients/Clients";
+import Footer from "../components/sections/Footer/Footer";
 import Hero from "../components/sections/Hero/Hero";
+import Metrics from "../components/sections/Metrics/Metrics";
+import { Servicioss } from "../components/sections/Servicios/Servicios";
 
 export default function Home() {
   return (
     <>
       <section>
         <Hero />
-        {/* <Clients /> */}
+        <Clients/>
+        <Servicioss/>
+        <Metrics/>
+        <Certifications/>
+        <Footer/>
       </section>
     </>
   );
